9254978fd8889261ed43fcaf9ed20d9906a51863
unknown <mahjoubi.bilel@gmail.com>
2025-03-26 01:09:50 +0100

    shadown

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,12 +7,12 @@ import {
 } from "@/components/ui/select"
 
 import { Separator } from "@/components/ui/separator"
-
 import { IoSearchOutline } from "react-icons/io5";
 
+import Data from "@/data/Data";
 const Search = () => {
     return (
-        <div className="md:flex flex-col bg-white rounded-md items-center px-5 gap-10 w-[550px] lg:w-[60%] md:rounded-full md:flex-row p-2 md:p-5">
+        <div className="md:flex flex-col bg-white rounded-md items-center px-5 gap-10 w-[350px] md:w-[70vw] md:rounded-full md:flex-row p-2 md:p-5">
 
             <Select>
                 <SelectTrigger className="w-full outline-none focus:outline-none shadow-none text-lg border-none">
@@ -31,9 +31,10 @@ const Search = () => {
                     <SelectValue placeholder="Cars Makes" />
                 </SelectTrigger>
                 <SelectContent className="w-full -mt-4 ml-16 md:ml-0 md:mt-6">
-                    <SelectItem value="light">Light</SelectItem>
-                    <SelectItem value="dark">Dark</SelectItem>
-                    <SelectItem value="system">System</SelectItem>
+                    {/* Add car makes */}
+                    {Data.CarsMakes.map((maker) => (
+                        <SelectItem key={maker.id} value={maker.name}>{maker.name}</SelectItem>
+                    ))}
                 </SelectContent>
             </Select>
 
@@ -41,16 +42,17 @@ const Search = () => {
 
             <Select>
                 <SelectTrigger className="w-full outline-none focus:outline-none shadow-none text-lg border-none">
-                    <SelectValue placeholder="Theme" />
+                    <SelectValue placeholder="Pricing" />
                 </SelectTrigger>
                 <SelectContent className="w-full -mt-4 ml-16 md:ml-0 md:mt-6">
-                    <SelectItem value="light">Light</SelectItem>
-                    <SelectItem value="dark">Dark</SelectItem>
-                    <SelectItem value="system">System</SelectItem>
+                   {/* Add pricing */}
+                   {Data.Pricing.map((price) => (
+                        <SelectItem key={price.id} value={price.ammount}>{price.ammount}</SelectItem>
+                    ))}
                 </SelectContent>
             </Select>
-            <div className="md:-ml-6">
-            <IoSearchOutline className="text-[50px] linear rounded-full p-3 cursor-pointer text-white font-bold hover:scale-105 transition-all duration-300"/>
+            <div className="md:-ml-10 flex justify-center">
+            <IoSearchOutline className="text-[50px] linear rounded-full p-3 cursor-pointer text-white font-bold hover:scale-105 transition-all duration-300" />
             </div>
         </div>
     )
